0f310d3d829e9fdafb3982adf633727415cbac74
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-08-17 18:53:18 +0300

    import vineo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(setLocalTime, 1000));
 
