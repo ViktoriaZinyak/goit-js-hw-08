b20403a4b7a93e77c25659ca2b153f6d1664c59e
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-08-18 11:56:31 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,21 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+let storageCurrentTime = 0;
 
+setStorageTimeOnPlayer();
 player.on('timeupdate', throttle(setLocalTime, 1000));
 
 function setLocalTime(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+function setStorageTimeOnPlayer() {
+  const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (currentTime) {
+    storageCurrentTime = currentTime;
+  }
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input[name="email"]');
+const inputMessage = document.querySelector('textarea[name="message"]');
+const formValue = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const storageData = localStorage.getItem(LOCALSTORAGE_KEY);
+const obj = JSON.parse(storageData);
+
+setOutput();
+
+form.addEventListener('input', throttle(setDataToStorage, 500));
+form.addEventListener('submit', resetFormData);
+
+function setDataToStorage(e) {
+  const output = e.target.value;
+  formValue[e.target.name] = output;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
+}
+
+function resetFormData(e) {
+  e.preventDefault();
+  localStorage.clear();
+  form.reset();
+  console.log(obj);
+}
+// formValue.email = form.elements.email.value;
+// formValue.message = form.elements.message.value;
+// localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
+
+function setOutput() {
+  if (!storageData) {
+    return;
+  }
+  inputEmail.value = obj.email || '';
+  inputMessage.value = obj.message || '';
+}
+
+//   if (obj.email) {
+//     inputEmail.value = obj.email;
+//   }
+//   if (obj.message) {
+//     inputMessage.value = obj.message;
+//   }
